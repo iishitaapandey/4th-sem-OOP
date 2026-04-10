e20cbc295ea54465b5f6bec883137faf28171173
iishitaapandey <iishitaapandey@gmail.com>
2026-04-10 12:53:13 +0530

    Add StringBuilder example with append and performance note

---
+++ OOP-Assignment/Builder.java
@@ -0,0 +1,10 @@
+class Builder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+
+        sb.append(" Programming");
+
+        System.out.println("Result: " + sb);
+
+    }
+}
